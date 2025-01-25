6e38ad8805383d19ed71cd632bedeeeb1491797d
myu123 <maxyu.usa.6@gmail.com>
2025-01-25 22:56:01 +0000

    Add basic Rust project with Hello COMP423

---
+++ helloCOMP423/target/debug/deps/helloCOMP423-39fe19a0f8f2a0b9.d
@@ -0,0 +1,5 @@
+/workspaces/rust-project/helloCOMP423/target/debug/deps/helloCOMP423-39fe19a0f8f2a0b9: src/main.rs
+
+/workspaces/rust-project/helloCOMP423/target/debug/deps/helloCOMP423-39fe19a0f8f2a0b9.d: src/main.rs
+
+src/main.rs:
---
+++ helloCOMP423/target/debug/helloCOMP423.d
@@ -0,0 +1,1 @@
+/workspaces/rust-project/helloCOMP423/target/debug/helloCOMP423: /workspaces/rust-project/helloCOMP423/src/main.rs
